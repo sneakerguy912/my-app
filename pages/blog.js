4d11893ee6431ae96df40b0fd3058d4ca6ac1de8
sneakerguy912 <103158206+sneakerguy912@users.noreply.github.com>
2022-07-31 20:43:45 -0500

    ssss

---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Link href='/blog'>
+        <a>Blog</a>
+      </Link>
+    </div>
+  )
+}
